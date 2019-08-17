bb5f5e3e980322378cf9a893acaff184f46df07b
Fedor Indutny <fedor@indutny.com>
2019-08-17 01:18:24 -0700

    src: save progress

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <aside className='sidebar'>
+        <h6 className='title'>channels:</h6>
+        <ul>
+          <li>c1</li>
+          <li>c2</li>
+          <li>c3</li>
+        </ul>
+      </aside>
+
+      <div className='main'>
+        <div className='main-container'>
+          <header className='channel-info'>
+            Information
+          </header>
+          <section className='channel-messages'>
+            <h1>h1</h1>
+            <h2>h2</h2>
+            <h3>h3</h3>
+            <h4>h4</h4>
+            <h5>h5</h5>
+            <h6>h6</h6>
+          </section>
+          <footer className='channel-compose'>
+            <input
+              className='channel-compose-text'
+              type='text'
+              placeholder='Write a message'/>
+          </footer>
+        </div>
+      </div>
     </div>
   );
 }
