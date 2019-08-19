e173422b9dc33570ca9bbc9a0ad3b7b778003c12
Fedor Indutny <fedor@indutny.com>
2019-08-19 00:59:40 -0700

    app: use HashRouter to make page refresh work

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import { HashRouter as Router, Route, Redirect } from 'react-router-dom';
 
 import './App.css';
 
