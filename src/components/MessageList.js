ec9a5396610f5dd9e06b27d13d37a4e355eb6403
Fedor Indutny <fedor@indutny.com>
2019-08-19 01:10:38 -0700

    message-list: expand authors

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 
 import { loadMessages } from '../redux/actions';
@@ -7,6 +7,7 @@ import { keyToColor } from '../utils';
 import './MessageList.css';
 
 function MessageList({ channelId, channels, loadMessages } ) {
+  const [ expandAuthorFor, setExpandAuthorFor ] = useState(null);
   const channel = channels.get(channelId);
 
   // Load only once per channel
@@ -19,32 +20,53 @@ function MessageList({ channelId, channels, loadMessages } ) {
       return null;
     }
 
-    const displayPath = message.author.displayPath;
     const publicKeys = message.author.publicKeys;
+    const displayPath = message.author.displayPath.map((component, i) => {
+      const style = { color: keyToColor(publicKeys[i]) };
+      const name = component.trim().replace(/^#+/, '');
+      return <span
+        className='message-author-name'
+        style={style}>
+        {name}
+      </span>;
+    });
 
     const timestamp = new Date(message.timestamp * 1000);
     const time = timestamp.toLocaleTimeString();
 
     let author;
-    let style;
     let authorClass = 'message-author';
     if (displayPath.length === 0) {
       authorClass += ' message-author-root';
       author = `#${channel.name}`;
+    } else if (expandAuthorFor === message.hash) {
+      author = [];
+      for (const component of displayPath) {
+        author.push(component);
+        author.push(<span>&gt;</span>);
+      }
+      author.pop();
     } else {
       author = displayPath[displayPath.length - 1];
-      author = author.trim().replace(/^#+/, '');
-
-      const publicKey = publicKeys[publicKeys.length - 1];
-      style = { color: keyToColor(publicKey) };
     }
 
     const content = <div className='message-content-text'>
       {message.json.text || ''}
     </div>;
 
+    const expandAuthor = (e) => {
+      e.preventDefault();
+
+      // Toggle
+      if (expandAuthorFor === message.hash) {
+        setExpandAuthorFor(null);
+      } else {
+        setExpandAuthorFor(message.hash);
+      }
+    };
+
     return <div className='message' key={message.hash}>
-      <div className={authorClass} style={style}>{author}</div>
+      <div className={authorClass} onClick={expandAuthor}>{author}</div>
       <div className='message-content'>{content}</div>
       <div className='message-time'>{time}</div>
     </div>;
